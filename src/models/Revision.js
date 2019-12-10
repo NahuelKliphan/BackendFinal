c17cd015da2bd5152e25ba27f0dabc2d77466a0c
NahuelKliphan <49241891+NahuelKliphan@users.noreply.github.com>
2019-12-10 16:07:18 -0300

    Foreign key Taller y Vehiculo con Revisiones, borra en cascada

--- src/models/Revision.js
+++ src/models/Revision.js
@@ -1,5 +1,7 @@
-import Sequalize from 'sequelize';
+import Sequalize, { HasMany } from 'sequelize';
 import database from '../database/database';
+import Taller from '../models/Taller';
+import Vehiculo from './Vehiculo';
 
 const Revision = database.define('revisiones',{
     id:{
@@ -23,10 +25,12 @@ const Revision = database.define('revisiones',{
         type: Sequalize.DOUBLE
     },
     idTaller:{
-        type: Sequalize.INTEGER
+        type: Sequalize.INTEGER,
+        foreignKey: Taller.id
     },
     idVehiculo:{
-        type: Sequalize.INTEGER
+        type: Sequalize.INTEGER,
+        foreignKey: Vehiculo.id
     },
     tareasRealizar:{
         type: Sequalize.STRING
@@ -39,5 +43,18 @@ const Revision = database.define('revisiones',{
     }
 
 });
+
+Revision.belongsTo(Vehiculo,{
+    foreignKey: 'idVehiculo',
+    onDelete: 'CASCADE',
+});
+
+Revision.belongsTo(Taller,{
+
+    foreignKey: 'idTaller',
+    onDelete: 'CASCADE',
+
+});
+
 
 export default Revision
